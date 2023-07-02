e29d5418d0143dae55df0fe1801a0dd944f69641
HamdyIIbrahim <hamdyfarouk444@gmail.com>
2023-07-02 22:33:33 +0300

    change the path of app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter ,Routes,Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
-import "./app.css"
+import "./App.css"
 import Home from "./pages/home/home"
 import About from "./pages/about/about"
 import Portfolio from "./pages/portfolio/portfolio"
